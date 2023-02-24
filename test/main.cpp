74989a767ddf1061680d49ad22cd5044d352d72f
Bilandzia <126132965+Bilandzia@users.noreply.github.com>
2023-02-23 21:28:51 -0800

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -16,18 +16,18 @@ int main()
 {
     cout<<"Welcome to myShell"<<endl<<endl;
     pthread_t cmd; // thread identifier
-    pthread_attr_t attr;
+
     char test[50];
     int i = 1;
-    string dir = "dir";
+
     while (1)
     {
         fgets(test,50,stdin);//reads input from user
         char* temptest= test;
         string please = temptest;// creates a string of the user input before strtok alters it
-        const char* please2 = please.c_str();
         char* arg1 = strtok(temptest, " ");
         char* arg2 = strtok(NULL, " ");
+        string help(arg1);
         cout<<"COMMAND TYPED BY USER:"<<endl<<please<<endl<<endl<<"ARGUEMENT 0: "<<arg1<<endl;
         //<<"ARGUEMENT 1: "<<arg2<<endl;
         while(arg2 != NULL)
@@ -38,10 +38,74 @@ int main()
         i++;
         }//reads out the rest of the arguements
         // needs to check if its part of the list before calling the pthread***********
+        i = 1;
+        string dir = "dir ";
+        string m = static_cast<string>(arg1);
+        if(please.find("dir")!= std::string::npos||please.find("help")!= std::string::npos||please.find("vol")!= std::string::npos||please.find("path")!= std::string::npos||please.find("tasklist")!= std::string::npos||please.find("notepad")!= std::string::npos||please.find("echo")!= std::string::npos||please.find("color")!= std::string::npos||please.find("ping")!= std::string::npos)
+        {if(please.find("help")!= std::string::npos)
+            {
+            string hp = please;
+            string hel = please;
+            hel.append(hel.begin()+4,hel.end());
+            hp.append("man ",4);
+            please = hp+hel;
+            }
+            else if(please.find("path")!= std::string::npos)
+            {
+            string pt = please;
+            string pth = please;
+            pth.append(pth.begin()+4,pth.end());
+            pt.append("pwd ",4);
+            please = pt+pth;
+            }
+            else if(please.find("vol")!= std::string::npos)
+            {
+            string pt = please;
+            string pth = please;
+            pt.append("df -l",5);//
+            please = pt;
+            }
+            else if(please.find("tasklist")!= std::string::npos)
+            {
+            string pt = please;
+            string pth = please;
+            pt.append("ps  ",8);
+            please = pt+pth;
+            }
+            else if(please.find("notepad")!= std::string::npos)/*change params of these from notepad to ping*/
+            {
+            string pt = please;
+            string pth = please;
+            pt.append("vim ",8);//
+            please = pt;
+            }
+            else if(please.find("echo")!= std::string::npos)
+            {
+            string pt = please;
+            string pth = please;
+            pt.append("echo ",5);//
+            please = pt;
+            }
+            else if(please.find("color")!= std::string::npos)
+            {
+            string pt = please;
+            string pth = please;
+            pt.append("color ",6);//
+            please = pt;
+            }
+            else if(please.find("ping")!= std::string::npos)
+            {
+            string pt = please;
+            string pth = please;
+            pt.append("ping ",5);//
+            please = pt;
+            }
         pthread_create(&cmd,NULL,ahhh,&please);// creates thread
         pthread_join(cmd,NULL);// waits for thread to finish
-        if(please == "exit")
+        }
+        if(please.find("exit")!= std::string::npos)
         {
+        cout<<"Thanks for using myShell!";
         exit(0);
         abort();
         }
@@ -50,3 +114,5 @@ int main()
 
     return 0;
 }
+
+
